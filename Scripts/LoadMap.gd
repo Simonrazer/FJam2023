5270d5433a83f0d817c7ddae229bf294671ba082
Simonrazer <lpsimonrazer@gmail.com>
2023-11-03 20:52:41 +0100

    make load work

--- Scripts/LoadMap.gd
+++ Scripts/LoadMap.gd
@@ -7,7 +7,7 @@ var tile_height = 10
 var tile_scene
 
 func _ready():
-	tile_scene = preload("res://Assets/tile.tscn")
+	tile_scene = preload("res://Prefabs/tile.tscn")
 	load_file(file)
 
 func load_file(file):
@@ -18,7 +18,7 @@ func load_file(file):
 	for ch in content:
 		match ch:
 			'T':
-				addTile(width,height)
+				addTile(width,height, "b_")
 				width = width + 1
 			'\n':
 				height = height + 1
@@ -28,13 +28,13 @@ func load_file(file):
 	return
 
 #addsTile. width and height are integers. The conversion into world space is 
-func addTile(width,height):
+func addTile(width,height,type):
 	var color
-	var tile = tile_scene.toInstance()
+	var tile = tile_scene.instantiate()
 	#set color base on checkerboard pattern
-	if (width+height)%2 == 0:
-		tile.set_position()
-	else:
-		return#bathtubSpawn.set_position(spawner.get_position())
+	tile.position = Vector3(width, 0, height)
+	tile.init(type)
+	add_child(tile)
+#bathtubSpawn.set_position(spawner.get_position())
 	var debugtemplate = "{0} {1},{2}".format({0:width, 1:height, 2:color})
 	print(debugtemplate)
