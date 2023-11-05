8c4f4885b91646251a8c6f71b37f093c8e1571d4
Simonrazer <lpsimonrazer@gmail.com>
2023-11-05 14:56:50 +0100

    shop

--- Scripts/Shop.gd
+++ Scripts/Shop.gd
@@ -1,14 +1,57 @@
 extends Node2D
 
-var itemScene = preload("res://Prefabs/Item.tscn")
-
+var textNode: RichTextLabel
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var instance = itemScene.instantiate()
-	add_child(instance)
+	textNode = get_node("CanvasLayer/ItemDesc")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_item_1_gui_input(event):
+	textNode.text = "Rüstung +2"
+
+	pass # Replace with function body.
+
+
+func _on_item_2_gui_input(event):
+	textNode.text = "Angreifen 3 Felder quer"
+	pass # Replace with function body.
+
+
+func _on_item_3_gui_input(event):
+	textNode.text = " Lebenspunkte +2"
+	pass # Replace with function body.
+
+
+func _on_item_4_gui_input(event):
+	textNode.text = "Lebensraub +1 Heilung"
+	pass # Replace with function body.
+
+
+func _on_item_5_gui_input(event):
+	textNode.text = "Angreifen +2 Reichweite"
+	pass # Replace with function body.
+
+
+func _on_item_6_gui_input(event):
+	textNode.text = "Angreifen +1 Schaden"
+	pass # Replace with function body.
+
+
+func _on_item_7_gui_inputd(event):
+	textNode.text = "Heilen +1 Heilung"
+	pass # Replace with function body.
+
+
+func _on_item_8_gui_input(event):
+	textNode.text = "Bewegung +2"
+	pass # Replace with function body.
+
+
+func _on_item_7_gui_input(event):
+	pass # Replace with function body.
